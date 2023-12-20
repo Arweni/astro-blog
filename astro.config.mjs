9ce1d167a84221b4268ff263a04f9b790decd94c
arweni <chad5059x1@yahoo.com>
2023-12-20 08:11:33 -0500

    Added Preact and an astro island; random greeting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://scintillating-sundae-b8c0fc.netlify.app/',
+  integrations: [preact()]
 });
