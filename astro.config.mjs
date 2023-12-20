aa83f8108a008f6d6961c0cd664bfd0cc28c6eff
arweni <chad5059x1@yahoo.com>
2023-12-19 19:04:03 -0500

    added rss feed page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://scintillating-sundae-b8c0fc.netlify.app/',
+});
